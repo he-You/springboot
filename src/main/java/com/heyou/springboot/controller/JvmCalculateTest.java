82521c0f7e3fceae87bd958d0921416927615c23
heyou <heyou_0423@163.com>
2019-11-20 18:01:37 +0800

    新增test方法

---
+++ src/main/java/com/heyou/springboot/controller/JvmCalculateTest.java
@@ -0,0 +1,18 @@
+package com.heyou.springboot.controller;
+
+/**
+ * @author heyou
+ * @date 2019-11-20 17:15
+ */
+public class JvmCalculateTest {
+    public static void main(String[] args) {
+        calculate();
+    }
+
+    private static void calculate(){
+        int i = 3;
+        int k = 2;
+        int j = i*k;
+        System.out.println(j);
+    }
+}
