ad0f3d5fd7670dc29e67c3f451e8c066950b9161
heyou-macbook <heyou_0423@163.com>
2020-06-28 23:25:59 +0800

    add:SPI机制 demo

---
+++ src/main/java/com/heyou/springboot/service/impl/SpiMain.java
@@ -0,0 +1,22 @@
+package com.heyou.springboot.service.impl;
+
+
+import com.heyou.springboot.service.SpiService;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/6/28 22:49
+ */
+public class SpiMain {
+    public static void main(String[] args) {
+        ServiceLoader<SpiService> loader = ServiceLoader.load(SpiService.class);
+        Iterator<SpiService> it = loader.iterator();
+        for(SpiService service : loader){
+            service.hello();
+            service.printText("hello spi");
+        }
+    }
+}
---
+++ src/main/java/com/heyou/springboot/service/impl/SpiServiceFirstImpl.java
@@ -0,0 +1,23 @@
+package com.heyou.springboot.service.impl;
+
+
+import com.heyou.springboot.service.SpiService;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/6/28 22:40
+ */
+public class SpiServiceFirstImpl implements SpiService {
+    public SpiServiceFirstImpl() {
+    }
+
+    @Override
+    public void printText(String text) {
+        System.out.println("this is first implements:"+text);
+    }
+
+    @Override
+    public void hello() {
+        System.out.println("hello");
+    }
+}
